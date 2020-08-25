173dc04f278167a50aa61629c85391680448a23b
Bryan <blindaa121@gmail.com>
2020-08-25 13:23:03 -0700

    Create Story Schema

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,9 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     }
 )
 
+// Logout User 
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+})
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 // Login/Landing Page route 
 // GET / 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     })
@@ -11,8 +11,10 @@ router.get('/', (req, res) => {
 
 // Dashboard Page route 
 // GET /dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, (req, res) => {
+    res.render('dashboard', {
+        name: req.user.firstName
+    })
 })
 
 module.exports = router;
